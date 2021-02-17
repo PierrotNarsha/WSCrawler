cb269ddd4e8ebbfe6ea436a35779a305a2b4bf7d
PierrotNarsha <narsha1001@gmail.com>
2021-02-17 18:57:13 +0900

    add function Data classification task

--- main.py
+++ main.py
@@ -11,24 +11,56 @@ papago = "https://papago.naver.com/?sk=ja&tk=ko&hn=0&st="
 def read_CardData():
     response = req.get(url)
 
+    data = json
+
     if response.status_code == 200:
 
         html = response.text
         soup = bts(html, 'html.parser')
         card = soup.find(id='cardDetail').find("tbody")
 
-        first = card.find_next()
-        card_name = first.find_next('img')['alt']
-        card_img_url = first.find_next('img')['src']
+        # first = card.find_next()
+        # card_name = first.find_next('img')['alt']
+        # card_img_url = first.find_next('img')['src']
 
-        for th in card.find_all('th'):
-            print(th)
+        tr_count = len(card.find_all('tr'))
 
-        for td in card.find_all('td'):
-            print(td)
+        cnt1, cnt2, cnt3, = 0, 0, 0
 
         for tr in card.find_all('tr'):
-            print(tr)
+            #if tr['class'] is 'first':
+            #   print(tr.find_next('img')['src'])
+            th_count = len(tr.find_all('th'))
+            td_count = len(tr.find_all('td'))
+
+            if th_count is 1 and td_count is 1:
+                cnt1 += 1
+                if tr.find('th') is 'カード番号':
+
+                elif tr.find('th') is '商品名':
+                elif tr.find('th') is 'ネオスタンダード区分':
+                elif tr.find('th') is '特徴':
+                elif tr.find('th') is 'テキスト':
+                elif tr.find('th') is 'フレーバー':
+
+            elif th_count is 2 and td_count is 2:
+                cnt2 += 1
+
+                th_all = tr.find_all('th')
+                td_all = tr.find_all('td')
+
+
+
+
+            elif th_count is 1 and td_count is 2:
+                cnt3 += 1
+                card_img_url = tr.find('img')['src']
+                card_name = tr.find_all('td')[1].get_text()
+
+        if cnt1 + cnt2 + cnt3 is tr_count:
+            print('true')
+        else:
+            print('false')
 
 if __name__ == '__main__':
     read_CardData()
